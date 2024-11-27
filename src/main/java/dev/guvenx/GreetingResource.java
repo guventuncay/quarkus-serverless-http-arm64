bb8d5c6a739a45fc454defce1e1c820ed321c24e
guventuncay <guven.tuncay01@gmail.com>
2024-11-27 23:51:52 +0300

    implement security with oidc

--- src/main/java/dev/guvenx/GreetingResource.java
+++ src/main/java/dev/guvenx/GreetingResource.java
@@ -3,14 +3,19 @@ package dev.guvenx;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.SecurityContext;
 
 @Path("/hello")
 public class GreetingResource {
 
+    @Context
+    SecurityContext context;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hello jaxrs";
+        return "hello %s".formatted(context.getUserPrincipal().getName());
     }
 }
--- src/test/java/dev/guvenx/GreetingTest.java
+++ src/test/java/dev/guvenx/GreetingTest.java
@@ -1,19 +1,24 @@
 package dev.guvenx;
 
 import io.quarkus.test.junit.QuarkusTest;
-import io.restassured.RestAssured;
+import io.quarkus.test.security.TestSecurity;
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.Matchers.equalTo;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
 @QuarkusTest
-class GreetingTest
-{
+class GreetingTest {
+
     @Test
-    void testJaxrs() {
-        RestAssured.when().get("/hello").then()
-                .contentType("text/plain")
-                .body(equalTo("hello jaxrs"));
+    @TestSecurity(user = "testUser", roles = {"admin", "user"})
+    void testHelloEndpointWithJWTAuth() {
+        given()
+                .when()
+                .get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("hello testUser"));
     }
 
 }
