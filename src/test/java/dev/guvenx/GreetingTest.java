cd1df38bbe69ecb40523949a503291372686a004
guventuncay <guven.tuncay01@gmail.com>
2024-11-27 21:19:30 +0300

    remove other frameworks than jax-rs

--- src/test/java/dev/guvenx/GreetingTest.java
+++ src/test/java/dev/guvenx/GreetingTest.java
@@ -7,34 +7,13 @@ import org.junit.jupiter.api.Test;
 import static org.hamcrest.Matchers.equalTo;
 
 @QuarkusTest
-public class GreetingTest
+class GreetingTest
 {
     @Test
-    public void testJaxrs() {
+    void testJaxrs() {
         RestAssured.when().get("/hello").then()
                 .contentType("text/plain")
                 .body(equalTo("hello jaxrs"));
-    }
-
-    @Test
-    public void testServlet() {
-        RestAssured.when().get("/servlet/hello").then()
-                .contentType("text/plain")
-                .body(equalTo("hello servlet"));
-    }
-
-    @Test
-    public void testVertx() {
-        RestAssured.when().get("/vertx/hello").then()
-                .contentType("text/plain")
-                .body(equalTo("hello vertx"));
-    }
-
-    @Test
-    public void testFunqy() {
-        RestAssured.when().get("/funqyHello").then()
-                .contentType("application/json")
-                .body(equalTo("\"hello funqy\""));
     }
 
 }
